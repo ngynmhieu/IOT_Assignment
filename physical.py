b75c35694b61e5f26b8c702ccd07d0475404f3eb
ngynmhieu <ngyn.mhieu@gmail.com>
2024-05-04 18:10:14 +0700

    test

--- physical.py
+++ physical.py
@@ -52,21 +52,21 @@ def serial_read_data(ser):
 soil_temperature =[1, 3, 0, 6, 0, 1, 100, 11]
 def readTemperature():
     serial_read_data(ser)
-    # ser.write(soil_temperature)
+    ser.write(soil_temperature)
     time.sleep(1)
     return serial_read_data(ser)
 
 soil_moisture = [1, 3, 0, 7, 0, 1, 53, 203]
 def readMoisture():
     serial_read_data(ser)
-    # ser.write(soil_moisture)
+    ser.write(soil_moisture)
 
     time.sleep(1)
     return serial_read_data(ser)
 
 while True:
     print("TEST SENSOR")
-    print(readMoisture())
+    print(f'The value of moisture is {readMoisture()}')
     time.sleep(1)
-    print(readTemperature())
+    print(f'The value of temp is {readTemperature()}')
     time.sleep(1)
