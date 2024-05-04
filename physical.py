967a15249710b6ba5d7cc4245dc9014b3a8bbb6a
ngynmhieu <ngyn.mhieu@gmail.com>
2024-05-04 18:02:26 +0700

    tesy

--- physical.py
+++ physical.py
@@ -40,7 +40,7 @@ def serial_read_data(ser):
     if bytesToRead > 0:
         out = ser.read(bytesToRead)
         data_array = [b for b in out]
-        print(data_array)
+        # print(data_array)
         if len(data_array) >= 7:
             array_size = len(data_array)
             value = data_array[array_size - 4] * 256 + data_array[array_size - 3]
@@ -51,16 +51,16 @@ def serial_read_data(ser):
 
 soil_temperature =[1, 3, 0, 6, 0, 1, 100, 11]
 def readTemperature():
-    # serial_read_data(ser)
+    serial_read_data(ser)
     ser.write(soil_temperature)
     time.sleep(1)
     return serial_read_data(ser)
 
 soil_moisture = [1, 3, 0, 7, 0, 1, 53, 203]
 def readMoisture():
-    # serial_read_data(ser)
+    serial_read_data(ser)
     ser.write(soil_moisture)
-    
+
     time.sleep(1)
     return serial_read_data(ser)
 
